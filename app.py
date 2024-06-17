51f709a870f46e0cc7f2d2d4fdb6a762db7f61a8
DarkAngel <marcelo@darkblue.tv>
2024-06-17 03:06:57 +0000

    html wip looking good

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+import os
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+# create the Flask app
+app = Flask(__name__, template_folder='html')
+socketio = SocketIO(app)
+
+
 from lib.song import Song
 
 # load the song from the JSON file
@@ -6,3 +15,13 @@ song = Song.from_json_file('songbook/obsession.json')
 
 # print the duration of the song
 print(song.song_duration)
+
+## Flask routes
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', song=song)
+
+## Main loop
+if __name__ == '__main__':
+    DEBUG = os.getenv('DEBUG', 'True') == 'True'
+    socketio.run(app, host="0.0.0.0", debug=DEBUG, port=5000, allow_unsafe_werkzeug=True)
